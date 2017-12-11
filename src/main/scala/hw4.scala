06a7608d74fdfe0141c2d9295722fc8c12badccd
Nogimon <x3722p@gmail.com>
2017-12-11 17:39:54 -0500

    first day

--- src/main/scala/hw4.scala
+++ src/main/scala/hw4.scala
@@ -1,3 +1,21 @@
 //hw4
-val list = Source.fromFile(".sql").getLine().toList
+import scala.io.Source
 
+object Hw4{
+
+    def main(args: Array[String]): Unit = {
+
+        println("hello")
+        val list = Source.fromFile("top40.sql").getLines().toList
+
+        println(list.head)
+        val splitstring = list.head.split("\'")
+        splitstring.foreach(println)
+        //println(splitstring(9))
+        val tuple1 = (splitstring(1), splitstring(3), splitstring(4)(2))
+        println(tuple1)
+        //println(splitstring.head)
+        println(list.tail.head)
+    }
+
+}
