f688095ea0182aa0880e1be49fba37b29e2d863b
Nogimon <x3722p@gmail.com>
2017-12-12 21:42:39 -0500

    read

--- src/main/scala/hw4.scala
+++ src/main/scala/hw4.scala
@@ -14,8 +14,37 @@ object Hw4{
         //println(splitstring(9))
         val tuple1 = (splitstring(1), splitstring(3), splitstring(4)(2))
         println(tuple1)
-        //println(splitstring.head)
-        println(list.tail.head)
+
+        
+        /*
+        val list1 = List(("a", "b", 2),2,3)
+        val list2 = List(3,("b",4))
+        val list3 = list1 ::: list2
+        println(list3)
+
+        var list11 = List(("a","b"),3)
+        list11 = list11 ::: List(("a","b","c"),4)
+        print(list11)
+        */
+
+        var stats = List[(Any)]()
+
+        for (list1 <- list)
+        {
+            //split every line of the list by ', and then pick up the three needed elements
+            val splitstring = list1.split("\'")
+            stats = stats ::: List((splitstring(1), splitstring(3), splitstring(4)(2)))
+            //println(stats)
+        }
+        println("stats")
+        println(stats)
+        //stats.foreach(println)
+
+
+        for (stat <- stats)
+        {
+            
+        }
     }
 
 }
