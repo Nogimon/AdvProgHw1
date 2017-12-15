1f95920ac6f1906a3c9635cb5f359db595ec49d9
Nogimon <x3722p@gmail.com>
2017-12-14 20:17:05 -0500

    finish read into map

--- src/main/scala/hw4.scala
+++ src/main/scala/hw4.scala
@@ -29,25 +29,53 @@ object Hw4{
 
         var stats = List[(String, String, Any)]()
 
+        var wordcount = Map("the" -> 2)
+
         for (list1 <- list)
         {
             //split every line of the list by ', and then pick up the three needed elements
             val splitstring = list1.split("\'")
             stats = stats ::: List((splitstring(1), splitstring(3), splitstring(4)(2)))
             //println(stats)
+
+            val wordcount1 = splitstring(3).split("\\W+").foldLeft(Map.empty[String, Int]){
+                (count, word) => count + (word -> (count.getOrElse(word, 0) + 1))
+            }
+            
+            wordcount = wordcount ++ wordcount1.map{case(name, count) => name->(count + wordcount.getOrElse(name,0))}
+
+            /*
+            val merge = wordcount.toSeq ++ wordcount1.toSeq
+            val grouped = merge.groupBy(_._1)
+            wordcount = grouped.mapValues(_.map(_._2).toList)
+            */
+            
+
+
         }
         println("stats")
+
+        val wordcountsort = wordcount.toSeq.sortBy(_._2)
+
+        println(wordcountsort)
+        
         //println(stats)
         //stats.foreach(println)
-
-
+        
+        /*
         for (stat <- stats)
         {
             println(stat._2)
             //stat._2.groupBy((word:String) => word).mapValues(_.length)
-            val wordCount = stat._2.groupBy(w => w).mapValues(_.size)
-            println(wordCount)
+            //val wordcount = stat._2.groupBy(w => w.split(" ")).mapValues(_.size)
+            
+            val wordcount = stat._2.foldLeft(Map.empty[String, Int]){
+                (count, word) => count + (word -> (count.getOrElse(word, 0) + 1))
+            }
+            
+            println(wordcount)
         }
+        */
 
 
     }
